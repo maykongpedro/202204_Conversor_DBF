a02ebed12feeed7a0385d3290bc1308d0209678b
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-07-04 19:28:38 -0300

    building ui upload

--- app.R
+++ app.R
@@ -3,8 +3,6 @@ library(shiny)
 
 # To make it easier to understand how to use the app, I used sidebarLayout() 
 # to divide the app into three main steps:
-
-
 # 1.Uploading and parsing the file
 # 2.Chosing a option to transform the file.
 # 3.Downloading the file
@@ -12,14 +10,42 @@ library(shiny)
 
 # 1.Uploading and parsing the file ----------------------------------------
 ui_upload <- sidebarLayout(
+  sidebarPanel = sidebarPanel(
     
+    fileInput(
+      inputId = "arquivo",
+      label = "Carregue um arquivo excel para converter",
+      multiple = FALSE,
+      accept = c("xlsx", "xls")
+    ),
+    
+    numericInput(
+      inputId = "linhas",
+      label = "Linhas para mostrar na pré-visualização",
+      value = 5,
+      min = 1,
+      max = 10
+    )
+    
+  ),
+  
+  mainPanel = mainPanel(
+    h3("Pré-visualização dos dados carregados:"),
+    tableOutput(outputId = "pre_visualiacao")
+  )
 )
 
 
 
 # 2.Chosing a option to transform the file --------------------------------
 ui_transform <- sidebarLayout(
+    sidebarPanel = sidebarPanel(
+      
+    ),
     
+    mainPanel = mainPanel(
+      
+    )
 )
 
 
@@ -41,6 +67,16 @@ ui <- fluidPage(
 # Server ------------------------------------------------------------------
 
 server <- function(input, output, session) {
+  
+
+  # Upload ------------------------------------------------------------------
+  raw_data <- reactive({
+    
+    # require file input
+    req(input$arquivo)
+    
+  })
+  
   
 }
 
