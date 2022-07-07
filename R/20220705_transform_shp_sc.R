eceea41e5dc7d73dfd2dad7a19672100ae1dfdce
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-07-06 23:36:25 -0300

    finished the function to transform sc shape

--- R/20220705_transform_shp_sc.R
+++ R/20220705_transform_shp_sc.R
@@ -10,4 +10,15 @@
 #' @examples
 transformar_shp_sc <- function(database) {
     
+    db <- database |>
+        dplyr::mutate(
+            REMSOFT_ID = as.integer(REMSOFT_ID),
+            ACTION = as.integer(ACTION),
+            CUT_PERIOD = as.integer(CUT_PERIOD),
+            THEME11 = as.character(THEME11),
+            IDADE = as.integer(IDADE)
+        ) |> 
+        as.data.frame()
+    
+    return(db)
 }
